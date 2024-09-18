6d5f7253c8c683b595fd21c1b13c61740eba6a36
Skalle9802 <christian@interpres.dk>
2024-09-18 15:18:08 +0200

    Pushing exercise 2, reformatting repo

---
+++ Setup.sh
@@ -0,0 +1,7 @@
+# any python packages you want to have preinstalled
+#pip install pingouin
+#pip install lxml
+
+# your git config information
+git config --global user.email christian@interpres.dk
+git config --global user.name Skalle9802
